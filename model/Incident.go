f334ef67597ae5d9607d9c76c62e7feb150acf34
Ben Zion Fischer <benzionfisher@gmail.com>
2024-10-15 00:17:17 +0300

    Split incident and incidentType structs

--- model/Incident.go
+++ model/Incident.go
@@ -20,26 +20,3 @@ func NewIncident(ip net.IP, incidentType IncidentType, timestamp time.Time) *Inc
 		Timestamp: timestamp,
 	}
 }
-
-// Incident type enum
-type IncidentType int
-
-const (
-	PortScanning       IncidentType = iota // 0
-	DDoSAttack                             // 1
-	LargeVolumeTraffic                     // 2
-)
-
-// String method for better readability
-func (it IncidentType) String() string {
-	switch it {
-	case PortScanning:
-		return "Port Scanning"
-	case DDoSAttack:
-		return "DDoS Attack"
-	case LargeVolumeTraffic:
-		return "Large Volume Traffic"
-	default:
-		return "Unknown Incident"
-	}
-}
---
+++ model/incident_type.go
@@ -0,0 +1,24 @@
+package model
+
+// Incident type enum
+type IncidentType int
+
+const (
+	PortScanning       IncidentType = iota // 0
+	DDoSAttack                             // 1
+	LargeVolumeTraffic                     // 2
+)
+
+// String method for better readability
+func (it IncidentType) String() string {
+	switch it {
+	case PortScanning:
+		return "Port Scanning"
+	case DDoSAttack:
+		return "DDoS Attack"
+	case LargeVolumeTraffic:
+		return "Large Volume Traffic"
+	default:
+		return "Unknown Incident"
+	}
+}
